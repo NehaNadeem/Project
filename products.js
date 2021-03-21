df7d35d69df57a3021f9c1eaacc142b938d98c8c
nehanadeem <n26.nadeem@gmail.com>
2021-03-21 16:42:26 +0300

    init commit

---
+++ products.js
@@ -0,0 +1,104 @@
+let arr = [
+  {
+    "id": 2001,
+    "subject": "Art",
+    "description": "The term art encompasses diverse media such as painting, sculpture, printmaking, drawing, decorative arts, photography, and installation..",
+    "location": "Jeddah",
+    "price": 20.00,
+    "image": "file:///C:/Users/Neha%20Nadeem/Desktop/MDX%20UNI%20DOC/Web%20App-%20Chinnu/project/images/art.jpg",
+    "availableInventory": 10,
+    "rating": 2
+  },
+  {
+    "id": 2002,
+    "subject": "English",
+    "description": "Learn vocabulary, terms, and more with flashcards, games, and other study tools.",
+    "location": "Riyadh",
+    "price": 11.00,
+    "image": "file:///C:/Users/Neha%20Nadeem/Desktop/MDX%20UNI%20DOC/Web%20App-%20Chinnu/project/images/eng.jpg",
+    "availableInventory": 12,
+    "rating": 4
+  },
+  {
+    "id": 2003,
+    "subject": "Geography",
+    "description": "Geography is the study of places and the relationships between people and their environments.",
+    "location": "Jeddah",
+    "price": 3.30,
+    "image": "file:///C:/Users/Neha%20Nadeem/Desktop/MDX%20UNI%20DOC/Web%20App-%20Chinnu/project/images/geog.jpg",
+    "availableInventory": 55,
+    "rating": 3
+  },
+  {
+    "id": 2004,
+    "subject": "History",
+    "description": "History describes our attempts to research, study and explain the past.",
+    "location": "Riyadh",
+    "price": 4.40,
+    "image": "file:///C:/Users/Neha%20Nadeem/Desktop/MDX%20UNI%20DOC/Web%20App-%20Chinnu/project/images/histry.jpg",
+    "availableInventory": 89,
+    "rating": 5
+  },
+  
+  {
+    "id": 2005,
+    "subject": "Math",
+    "description": "Mathematics is the science that deals with the logic of shape, quantity and arrangement.",
+    "location": "Dammam",
+    "price": 5.55,
+    "image": "file:///C:/Users/Neha%20Nadeem/Desktop/MDX%20UNI%20DOC/Web%20App-%20Chinnu/project/images/math.jpg",
+    "availableInventory": 10,
+    "rating": 4
+  },
+  {
+
+      "id": 2006,
+      "subject": "Music",
+      "description": "Music is a form of art that uses sound organised in time.",
+      "location": "Jeddah",
+      "price": 6.66,
+      "image": "file:///C:/Users/Neha%20Nadeem/Desktop/MDX%20UNI%20DOC/Web%20App-%20Chinnu/project/images/music.jpg",
+      "availableInventory": 11,
+      "rating": 5
+    },
+    {
+      "id": 2007,
+      "subject": "Physical Education",
+      "description": "Physical education is a course taught in school that focuses on developing physical fitness.",
+      "location": "Jeddah",
+      "price": 7.79,
+      "image": "file:///C:/Users/Neha%20Nadeem/Desktop/MDX%20UNI%20DOC/Web%20App-%20Chinnu/project/images/p.e.jpg",
+      "availableInventory": 12,
+      "rating": 5
+    },
+    {
+      "id": 2008,
+      "subject": "Photography",
+      "description": "Photography is the art, application, either electronically by means of an image sensor, or chemically by means of a light-sensitive material such as photographic film",
+      "location": "Abha",
+      "price": 8,
+      "image": "file:///C:/Users/Neha%20Nadeem/Desktop/MDX%20UNI%20DOC/Web%20App-%20Chinnu/project/images/photography.jpg",
+      "availableInventory": 13,
+      "rating": 5
+    },
+    {
+      "id": 2009,
+      "subject": "Science",
+      "description": "Science is the pursuit and application of knowledge and understanding of the natural and social world following a systematic methodology based on evidence.",
+      "location": "Khobar",
+      "price": 10,
+      "image": "file:///C:/Users/Neha%20Nadeem/Desktop/MDX%20UNI%20DOC/Web%20App-%20Chinnu/project/images/science.jpg",
+      "availableInventory": 14,
+      "rating": 5
+    },
+    {
+      "id": 2010,
+      "subject": "Statistics",
+      "description": "Descriptive Statistics are used to present quantitative descriptions in a manageable form. In a research study we may have lots of measures.",
+      "location": "Yanbu",
+      "price": 8.00,
+      "image": "file:///C:/Users/Neha%20Nadeem/Desktop/MDX%20UNI%20DOC/Web%20App-%20Chinnu/project/images/statistics.jpg",
+      "availableInventory": 15,
+      "rating": 5
+    }
+]
